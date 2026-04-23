602799e121d62eacdc5d7890b081bc9b7cb64d91
Ilian Zapryanov <ilian.iliev200@gmail.com>
2026-04-23 16:10:22 +0300

    rename project to orchestrix

--- src/test/java/com/orchestrix/OrchestrixApplicationTests.java
+++ src/test/java/com/orchestrix/OrchestrixApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.orchestrix;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,7 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest(properties = {
 		"spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration,org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration"
 })
-class DemoApplicationTests {
+class OrchestrixApplicationTests {
 
 	@Test
 	void contextLoads() {
